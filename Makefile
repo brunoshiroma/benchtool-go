3ce724e777c81e8771526b64154a9970e76104c9
Bruno Shiroma <brunoshiroma@gmail.com>
2025-10-14 00:04:20 +0000

    feat: impl lib for use in benchtool-android

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 #ANDROID_SDK=/SDK_HOME
-#NDK_BIN=$(ANDROID_SDK)/ndk/23.1.7779620/toolchains/llvm/prebuilt/linux-x86_64/bin
+#NDK_BIN=/workspaces/benchtool-go/android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin
 
 build:
 	go build cmd/benchtool-go/main.go
@@ -33,7 +33,7 @@ android-386:
 android-x64:
 	CGO_ENABLED=1 \
 	GOOS=android \
-	GOARCH=386 \
+	GOARCH=amd64 \
 	CC=$(NDK_BIN)/x86_64-linux-android31-clang \
 	go build -buildmode=c-shared -o benchtool-go-lib-android-x64.so pkg/lib.go
 
--- pkg/lib.go
+++ pkg/lib.go
@@ -1,13 +1,51 @@
 package main
 
-import "C"
-import "fmt"
+import (
+	"C"
+	"fmt"
+	"math/big"
+	"os"
+	"sort"
+
+	"github.com/brunoshiroma/benchtool-go/pkg/fibonacci"
+)
 
 //export benchtoolGoCall
-func benchtoolGoCall(iteration, repeat, benchType int) *C.char {
-	return C.CString(fmt.Sprintf("%d %d %d", iteration, repeat, benchType))
+func benchtoolGoCall(iteration int, repeats int, bench_type int) *C.char {
+	var lastResult big.Int = big.Int{}
+	lastResult.SetInt64(0)
+	resultsTimes := make([]int64, 0, repeats)
+
+	var benchFunction func(int) (big.Int, int64)
+	if bench_type == 1 {
+		benchFunction = fibonacci.CalculateFibonacciLoop
+	} else {
+		print(fmt.Sprintf("benctype not found %v ", bench_type))
+	}
+
+	for count := 0; count < repeats; count++ {
+		result, execTime := benchFunction(iteration)
+
+		if (lastResult.Int64() != 0) && lastResult.Text(10) != result.Text(10) {
+			print(fmt.Sprintf("Result error %v", result))
+			os.Exit(-1)
+		} else if lastResult.Int64() == 0 {
+			lastResult = result
+		}
+		resultsTimes = append(resultsTimes, execTime)
+	}
+
+	var resultTimeSum int64 = 0
+
+	sort.Slice(resultsTimes, func(i, j int) bool { return resultsTimes[i] < resultsTimes[j] })
+	resultsTimes = resultsTimes[1 : repeats-1]
+
+	for _, resultTime := range resultsTimes {
+		resultTimeSum += resultTime
+	}
+	average := resultTimeSum / int64(repeats)
+
+	return C.CString(fmt.Sprintf("%v %v", average, lastResult.Text(10)))
 }
 
-func main() {
-
-}
+func main() {}
